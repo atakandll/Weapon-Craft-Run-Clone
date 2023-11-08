172329819a614a3b8553be9e4f735637054b9dde
atakandll <atakanccnar@outlook.com>
2023-11-08 23:28:11 +0300

    Speed Changer platform setup

---
+++ Assets/Scripts/Managers/SpeedChangerVisual.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace Managers
+{
+    public class SpeedChangerVisual : MonoBehaviour
+    {
+
+        #region Self Variables
+
+        #region Serialized Variables
+
+        [SerializeField] private Material material;
+
+
+        #endregion
+
+        #region Private Variables
+
+        private float value;
+
+
+        #endregion
+
+        #endregion
+
+        private void Update()
+        {
+            value -= Time.deltaTime;
+            material.mainTextureOffset = new Vector2(1, value);
+        }
+    }
+}
